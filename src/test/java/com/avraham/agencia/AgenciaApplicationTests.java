e35a8c20092beb73c3ffc6ec7fd73640ee380f00
Avraham <souzaavraham@proton.me>
2025-06-05 02:30:23 -0300

    first commit

---
+++ src/test/java/com/avraham/agencia/AgenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avraham.agencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
